6b4cf7fddf79b5c763c1c8726d4c7286e9b54d35
Gireg Roussel <giregroussel@free.fr>
2018-05-03 12:01:35 +0200

    Maj2 Tuto1

---
+++ FirstTestMulti/Assets/Script/Respawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+	public List<GameObject> checkpoints;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Joueur"))
+		{
+			foreach (var checkpoint in checkpoints)
+			{
+				
+			}
+		}
+
+		
+	}
+}
---
+++ FirstTestMulti/Assets/Script/TutoAffichage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoAffichage : MonoBehaviour
+{
+
+	public Canvas display;
+	
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Joueur"))
+		{
+			display.enabled = true;
+		}
+
+		
+	}
+
+
+	private void OnCollisionExit(Collision other)
+	{
+		if (other.gameObject.CompareTag("Joueur"))
+		{
+			display.enabled = true;
+		}
+	}
+}
